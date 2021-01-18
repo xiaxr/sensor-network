a49a38069baca10904df2f872fca93ab637839cb
Steven Giacomelli <stevegiacomelli@gmail.com>
2021-01-18 16:53:19 -0500

    tweak

--- hub/backend/config.py
+++ hub/backend/config.py
@@ -1,6 +1,7 @@
-from RF24 import RF24, RF24_2MBPS, RF24_CRC_16, RF24_PA_MAX
-
 import json
+from uuid import uuid4
+
+from RF24 import RF24, RF24_2MBPS, RF24_CRC_16, RF24_PA_MAX
 
 
 def _read_config():
@@ -8,7 +9,12 @@ def _read_config():
         return json.load(file)
 
 
-def get_radio():
+def _save_config(values):
+    with open("settings.json", mode="wt") as file:
+        return json.dump(values, file)
+
+
+def get_gateway():
     config = _read_config()
 
     radio = RF24(config["hub"]["ce_pin"], config["hub"]["csn_pin"])
@@ -23,13 +29,24 @@ def get_radio():
     radio.address_width = config["general"]["address_width"]
     radio.payloadSize = config["general"]["payload_size"]
 
-    return Gateway(radio, config["general"]["channel_range"])
+    if "gateway_id" not in config["hub"]:
+        config["hub"]["gateway_id"] = uuid4().hex
+
+    _save_config(config)
+
+    return Gateway(radio, config)
 
 
 class Gateway:
-    def __init__(self, radio, channel_range):
+    def __init__(self, radio, config):
         self._radio = radio
-        self._channel_range = channel_range
+        self._gateway_id = config["hub"]["gateway_id"]
+        self._channel_range = config["general"]["channel_range"]
+        self._config = config
+
+    @property
+    def gateway_id(self):
+        return self._gateway_id
 
     @property
     def start_channel(self):
@@ -46,6 +63,8 @@ class Gateway:
     @channel.setter
     def channel(self, ch):
         self._radio.channel = ch
+        self._config["hub"]["starting_channel"] = ch
+        _save_config(self._config)
 
     @property
     def is_pvariant(self):
@@ -57,11 +76,10 @@ class Gateway:
             if self._radio.available():
                 self._radio.read(0, 0)
             return 1 if good_signal else 0
-        return 0 if self._radio.testCarrier() else 1
+        return 1 if self._radio.testCarrier() else 0
 
     def start_listening(self):
         self._radio.startListening()
-    
+
     def stop_listening(self):
         self._radio.stopListening()
-    
--- hub/backend/scan.py
+++ hub/backend/scan.py
@@ -1,32 +1,37 @@
-from config import get_radio
-
-import time
+from time import sleep
+from config import get_gateway
 
 
 class Scanner:
-    def __init__(self, radio):
-        self._radio = radio
+    def __init__(self, gateway):
+        self._radio = gateway
 
     def find_new_channel(self, repeats):
         values = {}
-        for i in range(repeats):            
+        for i in range(repeats):
             for channel in range(self._radio.start_channel,
                                  self._radio.end_channel + 1):
-                print(f"Interation {i+1} of {repeats} -- Channel {channel} of [{self._radio.start_channel}-{self._radio.end_channel + 1}]             \r",
-                  end='',
-                  flush=True)
+                print(
+                    f"Interation {i+1} of {repeats} -- Channel {channel} of [{self._radio.start_channel}-{self._radio.end_channel + 1}]             \r",
+                    end='',
+                    flush=True)
 
                 self._radio.channel = channel
                 self._radio.start_listening()
-                time.sleep(0.10)
+                sleep(0.10)
                 self._radio.stop_listening()
                 if channel not in values:
                     values[channel] = 0
                 values[channel] += self._radio.check_signal()
 
-        return sorted(tuple(values.items()), key=lambda x: x[1])
+        return sorted(tuple(values.items()), key=lambda x: x[1],
+                      reverse=True)[0][0]
 
 
 if __name__ == "__main__":
-    scanner = Scanner(get_radio())
-    print(scanner.find_new_channel(10))
+    gateway = get_gateway()
+    print(f"Gateway - {gateway.gateway_id.}")
+    scanner = Scanner(gateway)
+    new_channel = scanner.find_new_channel(10)
+    print(f"New channel found {new_channel}")
+    gateway.channel = new_channel
