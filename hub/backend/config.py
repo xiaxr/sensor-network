56b9b9f3c46b05ce53283613a1db6d09ac498c29
Steven Giacomelli <stevegiacomelli@gmail.com>
2021-01-18 15:50:55 -0500

    test

--- hub/backend/config.py
+++ hub/backend/config.py
@@ -1,4 +1,4 @@
-import RF24
+from RF24 import RF24, RF24_2MBPS, RF24_CRC_16, RF24_PA_MAX
 
 import json
 
@@ -11,18 +11,19 @@ def _read_config():
 def get_radio():
     config = _read_config()
 
-    radio = RF24.RF24(config["hub"]["ce_pin"], config["hub"]["csn_pin"])
+    radio = RF24(config["hub"]["ce_pin"], config["hub"]["csn_pin"])
     if not radio.begin():
         raise RuntimeError("radio cannot be initialized")
 
-    radio.power_level = globals()[config["hub"]["power_level"]]
+    radio.setPALevel(globals()[config["hub"]["power_level"]])
     radio.channel = config["hub"]["starting_channel"]
 
-    radio.data_rate = globals()[config["hub"]["data_rate"]]
-    radio.crc_length = globals()[config["hub"]["crc_length"]]
+    radio.setDataRate(globals()[config["hub"]["data_rate"]])
+    radio.setCRCLength(globals()[config["hub"]["crc_length"]])
     radio.address_width = config["general"]["address_width"]
-    radio.payload_size = config["general"]["payload_size"]
+    radio.payloadSize = config["general"]["payload_size"]
 
     return radio
+
 
 radio = get_radio()
