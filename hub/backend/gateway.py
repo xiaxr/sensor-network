468425c1564d44ce9db78ace6c9a50e6d0ac138d
Steven Giacomelli <stevegiacomelli@gmail.com>
2021-01-20 19:51:18 -0500

    test

--- hub/backend/gateway.py
+++ hub/backend/gateway.py
@@ -106,7 +106,7 @@ class Gateway:
 
     def pipe_address(self, node, pipe):
         address_translation = [0xc3, 0x3c, 0x33, 0xce, 0x3e, 0xe3, 0xec]
-        out = list(0xCCCCCCCCCC0000.to_bytes(8, byteorder='big', signed=False))
+        out = list(0xCCCCCCCCCC000000.to_bytes(8, byteorder='big', signed=False))
         count = 1
         dec = node
         while dec:
